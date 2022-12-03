0acf9f26efa21af4144ce83a078ba4c8e0f71192
Marcos Jonatan Suriani <4429720+jonatansuriani@users.noreply.github.com>
2022-12-03 14:48:29 -0300

    adjusted brewery parser

--- beercrawler/beercrawler/spiders/beeradvocate.py
+++ beercrawler/beercrawler/spiders/beeradvocate.py
@@ -3,8 +3,11 @@ import scrapy
 class BeerAdvocateSpider(scrapy.Spider):
     name = 'beeradvocate'
     allowed_domains = ['www.beeradvocate.com']
-    start_urls = ['https://www.beeradvocate.com/beer/styles/']
 
+    def start_requests(self):
+        url = 'https://www.beeradvocate.com/beer/styles/'
+        
+        yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
 
@@ -16,7 +19,7 @@ class BeerAdvocateSpider(scrapy.Spider):
             content_xpath = '//div[@id="ba-content"]/div[1]';
             return response.xpath(content_xpath).xpath(query).get(default='').strip()
 
-        beers_page = response.xpath("//tr//td//a[contains(@href, '/beer/profile/')]")
+        beers_page = response.xpath("//tr//td//a[contains(@href, '/beer/profile/')][1]")
         yield from response.follow_all(beers_page, callback=self.parse_beer)
 
         yield {
@@ -31,7 +34,9 @@ class BeerAdvocateSpider(scrapy.Spider):
         }
 
     def parse_beer(self, response):
-        yield from response.follow_all(response.xpath("//dt[contains(.,'From:')]/following-sibling::dd[1]/a/@href"), callback=self.parse_brewer)
+        brewery_url = response.urljoin(response.xpath("//dt[contains(.,'From:')]/following-sibling::dd[1]/a/@href").get())
+
+        yield response.follow(brewery_url, callback=self.parse_brewery)
 
         yield {
             'type' : 'beer',
@@ -40,18 +45,28 @@ class BeerAdvocateSpider(scrapy.Spider):
                 'name': response.css('h1::text').get(),
                 'images': response.xpath('//div[@id="main_pic_norm"]/div/img').getall(),
                 'brewery': {
-                    'original_url': response.urljoin(response.xpath("//dt[contains(.,'From:')]/following-sibling::dd[1]/a/@href").get()),
+                    'original_url': brewery_url,
                     'name': response.xpath("//dt[contains(.,'From:')]/following-sibling::dd[1]/a/b/text()").get()
                 }
             }
         }
 
-    def parse_brewer(self, response):
+    def parse_brewery(self, response):
 
         yield {
             'type' : 'brewery',
             'original_url': response.url,
             'doc':{
-                'name': response.css('h1::text').get()
+                'name': response.css('h1::text').get(),
+                'images': response.xpath('//div[@id="main_pic_norm"]/img/@src').getall(),
+                'address':{
+                    'address':  response.xpath('//div[@id="info_box"]/text()').get()[2:3],
+                    'zipcode':  response.xpath('//div[@id="info_box"]/text()').get()[4:5],
+                    'city': response.xpath('//div[@id="info_box"]/a/text()').getall()[0],
+                    'state': response.xpath('//div[@id="info_box"]/a/text()').getall()[1],
+                    'country': response.xpath('//div[@id="info_box"]/a/text()').getall()[2],
+                    'map': response.xpath('//div[@id="info_box"]/a/@href').getall()[3],
+                    'website': response.xpath('//div[@id="info_box"]/a/@href').getall()[4]
+                }
             }
         }
